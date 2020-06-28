a38454d929551882e49f39a677c2aae3eac5a3d0
ToXicGlo <53517791+ToXicGlo@users.noreply.github.com>
2020-06-28 14:46:21 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -9,7 +9,7 @@ AddEventHandler("ToXicGlo:Lockdown", function()
 	local user_id = vRP.getUserId({source})
 	local player = vRP.getUserSource({user_id})
 	if vRP.getInventoryItemAmount({user_id,"laptop_h"}) > 0 then
-		TriggerClientEvent('ToXicGlo:Lockdown', source)
+		TriggerClientEvent('ToXicGlo:Lockdown', -1)
 	else
 		TriggerClientEvent('mythic_notify:client:SendAlert', player, { type = 'error', text = 'You dont have any Hacker Laptop', length = '3000', style = {}})--TriggerClientEvent("pNotify:SendNotification", player,{text = "Y", type = "error", queue = "global", timeout = 4000, layout = "bottomCenter",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
 	end
@@ -23,7 +23,7 @@ AddEventHandler("ToXicGlo:NotifyPolice", function()
 		local xPlayer = vRP.getUserId({xPlayers[i]})
 		if vRP.hasPermission({xPlayer,"police.pc"}) then
 			TriggerClientEvent('mythic_notify:client:SendAlert', xPlayers[i], { type = 'inform', text = 'There is someone is trying to Lockdown the City!', length = '10000', style = {}})
-			TriggerClientEvent('ToXicGlo:setblip', source)
+			TriggerClientEvent('ToXicGlo:setblip', xPlayers[i])
 		end
 	end
 end)
